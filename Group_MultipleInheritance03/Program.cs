b0957bca506207c190e6f197ff2924e44c68ef4f
troubleman96 <itslugenge96@gmail.com>
2024-12-22 16:51:11 +0300

    first commit

---
+++ Group_MultipleInheritance03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Shape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Draw the shape.....");
+    }
+    
+}
+
+public class Triangle : Shape
+{
+    public void Hypothenus()
+    {
+        Console.WriteLine("The triangle's hypothenus = ");
+    }
+}
+
+public class ColoredTriangle : Triangle
+{
+    public void Color()
+    {
+        Console.WriteLine("The color of the triangle is pink>>>");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ColoredTriangle pinkTriangle = new ColoredTriangle();
+        pinkTriangle.Draw();
+        pinkTriangle.Hypothenus();
+        pinkTriangle.Color();
+    }
+}
